379eed852823f4847c9f0b576ac34d13545c287c
eramonb <eramonb@gmail.com>
2018-09-15 23:17:25 -0500

    comenta

---
+++ src/main/java/pe/com/gescom/foxgescom/FoxgescomApplication.java
@@ -0,0 +1,12 @@
+package pe.com.gescom.foxgescom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxgescomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoxgescomApplication.class, args);
+	}
+}
---
+++ src/test/java/pe/com/gescom/foxgescom/FoxgescomApplicationTests.java
@@ -0,0 +1,16 @@
+package pe.com.gescom.foxgescom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoxgescomApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
